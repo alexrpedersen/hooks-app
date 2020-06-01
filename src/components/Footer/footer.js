00679328e7a3643fd8fe84085a7b32557c173665
alexrpedersen <alex.r.pedersen@outlook.com>
2020-05-31 21:39:41 -0700

    final product

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './footer.scss';
+
+
+
+function Footer() {
+
+
+  return(
+    <>
+      <div className='footer'> Hook App By Alex Pedersen</div>
+    </>
+  )
+}
+
+export default Footer;
